c0fc5f73e46f60d368878f11112383e572ee9859
bikram@dey2003 <62688713+modhack2003@users.noreply.github.com>
2024-07-28 12:25:47 +0530

    imge fix

--- Travelhub/src/components/Header.jsx
+++ Travelhub/src/components/Header.jsx
@@ -1,27 +1,25 @@
-import React from 'react';
-import Button from './Button';
-import image from '../assets/image copy 7.png'
-import { SeeMoreButton } from '../constants/seemore';
-import ProgressBar from './progressBar/ProgressBar';
-import ProgressBarWrapper from './progressBar';
+import React from "react";
+import image from "../assets/image copy 7.webp";
+import { SeeMoreButton } from "../constants/seemore";
+import ProgressBarWrapper from "./progressBar";
 
 const Header = () => {
   return (
     <header className=" font-Poppins mb-  pb-12 h-screen  overflow-hidden ">
-      <div className='absolute top-44 left-5 w-[54%] h-auto pl-8 leading-snug'>
-        <h1 className=' text-white  text-6xl '>
+      <div className="absolute top-44 left-5 w-[54%] h-auto pl-8 leading-snug">
+        <h1 className=" text-white  text-6xl ">
           Discover the Beauty and the Tropical Heaven places around the globe
         </h1>
-        <p className='text-white mt-6 text-xl w-[75%]'>
-          we are dedicated to curating personalized itineraries that showcase Bali's breathtaking landscapes, multicultural, and authentic charm.
+        <p className="text-white mt-6 text-xl w-[75%]">
+          we are dedicated to curating personalized itineraries that showcase
+          Bali's breathtaking landscapes, multicultural, and authentic charm.
         </p>
       </div>
-      <div className=' mx-6 px-6 w-[97%] flex justify-between absolute gap-80 top-[80vh] '>
-      <SeeMoreButton />
-      <ProgressBarWrapper />      
+      <div className=" mx-6 px-6 w-[97%] flex justify-between absolute gap-80 top-[80vh] ">
+        <SeeMoreButton />
+        <ProgressBarWrapper />
       </div>
-      <img src={image} className='object-cover'  />
-
+      <img src={image} className="object-cover" />
     </header>
   );
 };
